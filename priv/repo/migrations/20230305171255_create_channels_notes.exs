370bdfe99dbc93de784fea379e3cfb1ff6846270
Inhji <johnnie@posteo.de>
2023-03-05 19:52:16 +0100

    remove timestamp and guid from channels_notes table

--- priv/repo/migrations/20230305171255_create_channels_notes.exs
+++ priv/repo/migrations/20230305171255_create_channels_notes.exs
@@ -2,11 +2,9 @@ defmodule Chiya.Repo.Migrations.CreateChannelsNotes do
   use Ecto.Migration
 
   def change do
-    create table(:channels_notes) do
+    create table(:channels_notes, primary_key: false) do
       add :channel, references(:channels, on_delete: :nothing)
       add :note, references(:notes, on_delete: :nothing)
-
-      timestamps()
     end
 
     create index(:channels_notes, [:channel])
