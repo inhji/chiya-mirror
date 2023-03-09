c9458f13349bf5e79d502689a833f3aadf0a8ff0
inhji <johnnie@posteo.de>
2023-03-09 15:37:10 +0100

    „lib/chiya_web/controllers/page_controller.ex“ ändern

--- lib/chiya_web/controllers/page_controller.ex
+++ lib/chiya_web/controllers/page_controller.ex
@@ -5,11 +5,11 @@ defmodule ChiyaWeb.PageController do
     # The home page is often custom made,
     # so skip the default app layout.
     settings = Chiya.Site.get_settings()
-    render(conn, :home, layout: false, settings: settings)
+    render(conn, :home, layout: {ChiyaWeb.Layouts, "public.html"}, settings: settings)
   end
 
   def channel(conn, %{"slug" => channel_slug}) do
     channel = Chiya.Channels.get_channel_by_slug_preloaded!(channel_slug)
-    render(conn, :channel, layout: false, channel: channel)
+    render(conn, :channel, layout: {ChiyaWeb.Layouts, "public.html"}, channel: channel)
   end
 end
