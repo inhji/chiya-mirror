ba81c7e351773aa06548d373baafe4d558745b5b
inhji <johnnie@posteo.de>
2023-03-10 12:49:06 +0100

    „lib/chiya_web/format.ex“ hinzufügen

---
+++ lib/chiya_web/format.ex
@@ -0,0 +1,14 @@
+def from_now_short(now \\ DateTime.utc_now(), later) do
+    diff = DateTime.diff(now, later)
+    cond do
+      diff <= -24 * 3600 -> "in #{div(-diff, 24 * 3600)}d"
+      diff <= -3600 -> "in #{div(-diff, 3600)}h"
+      diff <= -60 -> "in #{div(-diff, 60)}m"
+      diff <= -5 -> "in #{-diff}s"
+      diff <= 5 -> "now"
+      diff <= 60 -> "#{diff}s ago"
+      diff <= 3600 -> "#{div(diff, 60)}m ago"
+      diff <= 24 * 3600 -> "#{div(diff, 3600)}h ago"
+      true -> "#{div(diff, 24 * 3600)}d ago"
+    end
+end
