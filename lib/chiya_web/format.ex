17753ee67169bc9751e95bd3b073f5f7d4fa7357
Inhji <johnnie@posteo.de>
2023-03-13 03:27:17 +0100

    improve from_now/2

--- lib/chiya_web/format.ex
+++ lib/chiya_web/format.ex
@@ -1,11 +1,11 @@
 defmodule ChiyaWeb.Format do
-  def from_now(later) do
+  def from_now(%DateTime{} = later) do
     now = DateTime.utc_now()
     diff = DateTime.diff(now, later)
     do_from_now(diff)
   end
 
-  def from_now_naive(later) do
+  def from_now(%NaiveDateTime{} = later) do
     now = NaiveDateTime.utc_now()
     diff = NaiveDateTime.diff(now, later)
     do_from_now(diff)
@@ -13,15 +13,19 @@ defmodule ChiyaWeb.Format do
 
   def do_from_now(diff) do
     cond do
-      diff <= -24 * 3600 -> "in #{div(-diff, 24 * 3600)}d"
-      diff <= -3600 -> "in #{div(-diff, 3600)}h"
-      diff <= -60 -> "in #{div(-diff, 60)}m"
-      diff <= -5 -> "in #{-diff}s"
+      diff <= -24 * 3600 -> "in #{div(-diff, 24 * 3600)} days"
+      diff <= -3600 -> "in #{div(-diff, 3600)} hours"
+      diff <= -60 -> "in #{div(-diff, 60)} minutes"
+      diff <= -5 -> "in #{-diff} seconds"
       diff <= 5 -> "now"
-      diff <= 60 -> "#{diff}s ago"
-      diff <= 3600 -> "#{div(diff, 60)}m ago"
-      diff <= 24 * 3600 -> "#{div(diff, 3600)}h ago"
-      true -> "#{div(diff, 24 * 3600)}d ago"
+      diff <= 60 -> "#{diff} seconds ago"
+      diff <= 3600 -> "#{div(diff, 60)} minutes ago"
+      diff <= 24 * 3600 -> "#{div(diff, 3600)} hours ago"
+      true -> "#{div(diff, 24 * 3600)} days ago"
     end
+  end
+
+  def pretty_date(%NaiveDateTime{} = date) do
+    Calendar.strftime(date, "%d.%m.%Y")
   end
 end
