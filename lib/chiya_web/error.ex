d1032f6e6b5b58cb48059d1e6d9e48aef77c9e3c
Inhji <johnnie@posteo.de>
2023-03-14 23:48:00 +0100

    add error helper for controllers to generate error messages

---
+++ lib/chiya_web/error.ex
@@ -0,0 +1,11 @@
+defmodule ChiyaWeb.Error do
+	import Plug.Conn, only: [put_status: 2]
+	import Phoenix.Controller, only: [put_view: 2, render: 3]
+
+	def render_error(conn, :not_found, assigns \\ []) do
+		conn
+		|> put_status(:not_found)
+		|> put_view(ChiyaWeb.ErrorHTML)
+		|> render("404.html", assigns)
+	end
+end
