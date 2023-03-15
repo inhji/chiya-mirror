11cbe215c27e66c40cf1ddda8a4c3d930a886f2c
inhji <johnnie@posteo.de>
2023-03-15 16:04:51 +0100

    „lib/chiya_web/components/core_components.ex“ ändern

--- lib/chiya_web/components/core_components.ex
+++ lib/chiya_web/components/core_components.ex
@@ -9,9 +9,14 @@ defmodule ChiyaWeb.CoreComponents do
   Icons are provided by [heroicons](https://heroicons.com). See `icon/1` for usage.
   """
   use Phoenix.Component
+  use Phoenix.VerifiedRoutes,
+    endpoint: ChiyaWeb.Endpoint,
+    router: ChiyaWeb.Router,
+    statics: ChiyaWeb.static_paths()
 
   alias Phoenix.LiveView.JS
   import ChiyaWeb.Gettext
+  
   
   @doc """
   Renders a horizontal line
