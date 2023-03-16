de3daea40ec5b2ce75f553461fb69f18e0f4a7b8
Inhji <johnnie@posteo.de>
2023-03-16 07:30:58 +0100

    apply prose invert based on the theme variant \o/

--- lib/chiya_web/components/layouts.ex
+++ lib/chiya_web/components/layouts.ex
@@ -2,4 +2,11 @@ defmodule ChiyaWeb.Layouts do
   use ChiyaWeb, :html
 
   embed_templates "layouts/*"
+
+  @doc """
+  Defines which themes are light and which are dark themes.
+  This influences the prose styles in app.css. 
+  """
+  def theme_variant(:default), do: "light"
+  def theme_variant(:roguelight), do: "dark"
 end
