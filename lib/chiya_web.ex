585ffd5145979d888790914379c350ef487c1bf6
Inhji <johnnie@posteo.de>
2023-03-23 08:24:13 +0100

    add favicon \o/

--- lib/chiya_web.ex
+++ lib/chiya_web.ex
@@ -17,7 +17,7 @@ defmodule ChiyaWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon favicon.ico robots.txt browserconfig.xml site.webmanifest)
 
   def router do
     quote do
