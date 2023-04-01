eba003b3b5b2715771c3016f730a9d19468b887f
Inhji <johnnie@posteo.de>
2023-04-01 16:05:24 +0200

    clean up

--- lib/chiya_web/exim.ex
+++ lib/chiya_web/exim.ex
@@ -1,12 +1,12 @@
 defmodule ChiyaWeb.Exim do
   alias Chiya.Notes.Note
 
-  defp frontmatter(%Note{name: title, channels: channels}) do
+  defp create_frontmatter(%Note{name: title, channels: channels}) do
     channels_raw = Enum.map_join(channels, "], [", fn c -> "\"#{c.name}\"" end)
     "title: \"#{title}\"\ncategories: [#{channels_raw}]"
   end
 
   def export_note(%Note{content: content} = note) do
-    "---\n#{frontmatter(note)}\n---\n#{content}"
+    "---\n#{create_frontmatter(note)}\n---\n#{content}"
   end
 end
