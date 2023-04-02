4c68d4be5c18ff1b66624a3ce6f2fb489a775ef6
Inhji <johnnie@posteo.de>
2023-04-02 23:55:43 +0200

    fix forms

--- lib/chiya_web/components/core_components.ex
+++ lib/chiya_web/components/core_components.ex
@@ -249,7 +249,7 @@ defmodule ChiyaWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="space-y-8 bg-white dark:bg-gray-900 mt-10">
+      <div class="space-y-8 p-3 shadow rounded bg-white dark:bg-gray-900 mt-10">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
