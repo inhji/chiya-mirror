21f6142f66a6c3731c6b2275905c1fa18be5a8e9
Inhji <johnnie@posteo.de>
2023-06-11 09:13:04 +0200

    switch back to git plug_micropub

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Chiya.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19"},
       {:plug_cowboy, "~> 2.5"},
-      {:plug_micropub, path: "../plug_micropub"},
+      {:plug_micropub, git: "https://git.inhji.de/inhji/plug_micropub.git", ref: "main"},
       {:postgrex, ">= 0.0.0"},
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
