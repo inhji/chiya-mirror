a777c9a8b349611ae25bf0a02bb275d9a4541d67
Inhji <johnnie@posteo.de>
2023-07-04 22:46:00 +0200

    include internal link rendering

---
+++ test/chiya_web/markdown_test.exs
@@ -0,0 +1,25 @@
+defmodule ChiyaWeb.MarkdownTest do
+	use Chiya.DataCase
+
+	alias ChiyaWeb.Markdown
+
+	describe "render/1" do
+		test "renders simple markdown" do
+			html = Markdown.render("# Title")
+			assert html =~ "id=\"title\""
+			assert html =~ "Title"
+			assert html =~ "</h1>"
+		end
+
+		test "renders a link to a note" do
+			html = Markdown.render("[[foo]]")
+			assert html =~ "/note/foo"
+		end
+
+		test "renders a link to a note with custom title" do
+			html = Markdown.render("[[foo|MyFoo]]")
+			assert html =~ "/note/foo"
+			assert html =~ "MyFoo"
+		end
+	end
+end
