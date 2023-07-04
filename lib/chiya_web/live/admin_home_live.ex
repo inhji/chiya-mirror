c2785d014c6fac3a018f5794812bfc834af5caa2
Inhji <johnnie@posteo.de>
2023-07-04 06:57:32 +0200

    move logout button to profile

--- lib/chiya_web/live/admin_home_live.ex
+++ lib/chiya_web/live/admin_home_live.ex
@@ -51,14 +51,6 @@ defmodule ChiyaWeb.AdminHomeLive do
         <.link href={~p"/user"}>
           <.button>Profile</.button>
         </.link>
-        <.link
-          href={~p"/user/log_out"}
-          method="delete"
-          data-confirm="Do you want to logout?"
-          class="text-sm leading-6 text-gray-900 dark:text-gray-100 dark:hover:text-gray-300 hover:text-gray-700"
-        >
-          <.button>Log out</.button>
-        </.link>
       </:actions>
     </.header>
 
