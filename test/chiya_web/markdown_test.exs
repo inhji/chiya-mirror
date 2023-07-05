2ac2e1ec01c83143be7be62fdac178f9be9611a0
Inhji <johnnie@posteo.de>
2023-07-05 06:49:50 +0200

    add test for markdown

--- test/chiya_web/markdown_test.exs
+++ test/chiya_web/markdown_test.exs
@@ -1,25 +1,31 @@
 defmodule ChiyaWeb.MarkdownTest do
-	use Chiya.DataCase
+  use Chiya.DataCase
 
-	alias ChiyaWeb.Markdown
+  alias ChiyaWeb.Markdown
 
-	describe "render/1" do
-		test "renders simple markdown" do
-			html = Markdown.render("# Title")
-			assert html =~ "id=\"title\""
-			assert html =~ "Title"
-			assert html =~ "</h1>"
-		end
+  describe "render/1" do
+    test "renders simple markdown" do
+      html = Markdown.render("# Title")
+      assert html =~ "id=\"title\""
+      assert html =~ "Title"
+      assert html =~ "</h1>"
+    end
 
-		test "renders a link to a note" do
-			html = Markdown.render("[[foo]]")
-			assert html =~ "/note/foo"
-		end
+    test "renders a link to a note" do
+      html = Markdown.render("[[foo]]")
+      assert html =~ "/note/foo"
+    end
 
-		test "renders a link to a note with custom title" do
-			html = Markdown.render("[[foo|MyFoo]]")
-			assert html =~ "/note/foo"
-			assert html =~ "MyFoo"
-		end
-	end
+    test "renders a link to a note with custom title" do
+      html = Markdown.render("[[foo|MyFoo]]")
+      assert html =~ "/note/foo"
+      assert html =~ "MyFoo"
+    end
+
+    test "renders a link to a not existing note with custom class" do
+      html = Markdown.render("[[foo]]")
+      assert html =~ "/note/foo"
+      assert html =~ "class=\"invalid\""
+    end
+  end
 end
