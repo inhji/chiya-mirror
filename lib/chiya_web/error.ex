fe2acd9f022636b43f0654616a42cc6e919f9c2d
inhji <johnnie@posteo.de>
2023-07-07 10:13:55 +0200

    „lib/chiya_web/error.ex“ ändern

--- lib/chiya_web/error.ex
+++ lib/chiya_web/error.ex
@@ -9,12 +9,4 @@ defmodule ChiyaWeb.Error do
     |> put_view(ChiyaWeb.ErrorHTML)
     |> render("404.html", assigns)
   end
-
-  def render_error(conn, :not_found, assigns \\ []) do
-    conn
-    |> put_root_layout, {ChiyaWeb.Layouts, :root_error}
-    |> put_status(:not_found)
-    |> put_view(ChiyaWeb.ErrorHTML)
-    |> render("404.html", assigns)
-  end
 end
