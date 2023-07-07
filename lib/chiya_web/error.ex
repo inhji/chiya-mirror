9b5a9d18a82dace2ef22ccdb450e1c9a751746a8
inhji <johnnie@posteo.de>
2023-07-07 10:21:32 +0200

    „lib/chiya_web/error.ex“ ändern

--- lib/chiya_web/error.ex
+++ lib/chiya_web/error.ex
@@ -2,8 +2,6 @@ defmodule ChiyaWeb.Error do
   import Plug.Conn, only: [put_status: 2]
   import Phoenix.Controller, only: [put_view: 2, render: 3]
 
-  plug :put_root_layout, {ChiyaWeb.Layouts, :root_error}
-
   def render_error(conn, :not_found, assigns \\ []) do
     conn
     |> put_root_layout(html: {ChiyaWeb.Layouts, :root_error})
