8837b4c7b15f0ae12b247db4b716245ecf49c123
inhji <johnnie@posteo.de>
2023-07-13 10:28:15 +0200

    „assets/js/app.js“ ändern

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,12 +21,9 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import lolight from "../vendor/lolight"
 import React from "react"
 import { createRoot } from 'react-dom/client'
 import KBar from "./kbar"
-
-lolight("pre code")
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
