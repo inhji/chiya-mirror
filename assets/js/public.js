e90327fedd8445c4dad1ebe08bc2db84ab895018
Inhji <johnnie@posteo.de>
2023-07-20 07:20:21 +0200

    add tablesort

--- assets/js/public.js
+++ assets/js/public.js
@@ -1,29 +1,32 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import 'phoenix_html'
 
-import hljs from "highlight.js"
+import hljs from 'highlight.js'
 import GLightbox from 'glightbox'
+import Tablesort from 'tablesort'
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('pre code').forEach((el) => {
-    hljs.highlightElement(el);
-  });
+  document.querySelectorAll('.prose pre code').forEach((el) => 
+    hljs.highlightElement(el))
+
+  document.querySelectorAll('.prose table').forEach(el => 
+  	new Tablesort(el))
 });
 
 document
-	.querySelector("#dark-mode-toggle")
-	.addEventListener("click", (e) => {
+	.querySelector('#dark-mode-toggle')
+	.addEventListener('click', (e) => {
 		e.preventDefault()
 		const data = document.documentElement.dataset
-		if (data["mode"] && data["mode"] == "dark") {
-			delete data["mode"]
-			window.localStorage.removeItem("theme")
+		if (data['mode'] && data['mode'] == 'dark') {
+			delete data['mode']
+			window.localStorage.removeItem('theme')
 		} else {
-			data["mode"] = "dark"
-			window.localStorage.setItem("theme", "dark")
+			data['mode'] = 'dark'
+			window.localStorage.setItem('theme', 'dark')
 		}
 	})
 
-GLightbox({ selector: ".lightbox" })
+GLightbox({ selector: '.lightbox' })
 
 window.hljs = hljs
